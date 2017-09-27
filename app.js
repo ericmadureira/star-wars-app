a5e39d81bb980d126564f69e981e721f1eb37c27
EricMadureira <ericmadureira.uesc@gmail.com>
2017-09-27 08:13:16 -0300

    GET Request and Views

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/////////////////			SETUP			////////////////////////////
+
+// APP 
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
@@ -7,14 +10,32 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//////////////////			ROUTES			/////////////////////////
+
+// LANDING PAGE AND SEARCH FORM
 app.get("/", function(req, res){
-	res.render("index", {bork: "maximum borkdrive"});
+	res.render("index");
 });
 
+// DISPLAYS SEARCH RESULTS
+app.get("/results", function(req, res){
+	var query = req.query.search;
+	var url = "https://swapi.co/api/people/?search="+query;
+	request(url, function(error, response, body){
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.render("results", {data: data});
+		}
+	});
+});
+
+// CATCHES ROUTE EXCEPTIONS
 app.get("*", function(req, res){
 	res.render("page_not_found");
 });
 
+// SERVER
 app.listen("3001", function(){
   console.log("App on port 3001...");
 });
+/////////////////////////////////////////////////////////////
